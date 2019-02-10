945b0b56154c88c4689e4dbb7698a70ff86b87bf
fnuesse <felix.nuesse@t-online.de>
2019-02-10 10:07:03 +0100

    Updated Fragment-design
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- src/main/java/com/owncloud/android/ui/activity/SyncedFoldersActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/SyncedFoldersActivity.java
@@ -491,6 +491,7 @@ public class SyncedFoldersActivity extends FileActivity implements SyncedFolderA
                     false, getAccount().name,
                     FileUploader.LOCAL_BEHAVIOUR_FORGET, false, null, MediaFolderType.CUSTOM);
                 onSyncFolderSettingsClick(0, emptyCustomFolder);
+                break;
             }
 
             case R.id.action_edit_blacklist_patterns: {
@@ -498,6 +499,7 @@ public class SyncedFoldersActivity extends FileActivity implements SyncedFolderA
                 DialogFragment editor = PatternBlacklistEditorDialogFragment.newInstance(
                     R.string.blacklist_editor_exclude_pattern_title);
                 editor.show(getSupportFragmentManager(), "test");
+                break;
             }
 
             default:
